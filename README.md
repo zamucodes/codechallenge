9330e8b19e1ae46c93eb00a6b3b41a6c38c86840
Samuel Chavez <>
2023-01-26 17:43:34 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# codechallenge
-code challenge
+# Code challenge Swift UIKit / SwiftUI
+
+## Authors
+
+* **Samuel Chavez** - *Initial work* - [zamuset](https://github.com/zamucodes)
---
+++ codechallenge/codechallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  codechallenge
+//
+//  Created by Samuel Chavez on 26/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
